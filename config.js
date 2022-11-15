f081c2256469a5ed0b9b9320e46f45818cdb40a9
alittegift <118001233+alittegift@users.noreply.github.com>
2022-11-15 22:06:40 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'NHƯ QUỲNH';
+const giftUrl = 'https://alittegift.github.io/NhuQuynh/';
+const eventName = 'A LITTLE GIFT NÈE';
+const titleCard = 'Tặng ai đó';
+const contentCard = 'Chúc ai đó ngày mai thi thật tốt, bình tĩnh tự tin chiến thắng nè, hong cần phải sợ gì hết, đằng sau m vẫn luôn có người ủng hộ và bên cạnh m mờ, nên là cố lên nhaa <3';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'Q.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
